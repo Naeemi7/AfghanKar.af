20833f3dac24c4483b5863e8151c047711d44808
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-11-04 17:36:28 +0430

    the not-found page has been re-designed

--- client/src/pages/not-found-page/NotFoundPage.jsx
+++ client/src/pages/not-found-page/NotFoundPage.jsx
@@ -1,3 +1,4 @@
+import "@styles/pages/not-found.scss";
 import Button from "@reusable/Button";
 import useNavigation from "@hooks/useNavigation";
 import useUserContext from "@hooks/useUserContext";
@@ -8,23 +9,21 @@ const NotFoundPage = () => {
   const { isLoggedIn } = useUserContext();
 
   return (
-    <div className="main-container">
-      <div className="not-found-page-container">
-        <div className="not-found-content">
-          <h1>404</h1>
-          <span></span>
-          <p>Sorry, the page you are looking for could not be found</p>
-          <Button
-            name="Go to Main"
-            width={120}
-            onClick={() => {
-              isLoggedIn ? goTo("/home") : goTo("/");
-            }}
-          />
-        </div>
-        <div className="not-found-image">
-          <img src={notFoundImage} alt="Not Found image" />
-        </div>
+    <div className="not-found-page-container">
+      <div className="not-found-content">
+        <h1>404</h1>
+        <span></span>
+        <p>Sorry, the page you are looking for could not be found</p>
+        <Button
+          name="Go to Main"
+          width={120}
+          onClick={() => {
+            isLoggedIn ? goTo("/home") : goTo("/");
+          }}
+        />
+      </div>
+      <div className="not-found-image">
+        <img src={notFoundImage} alt="Not Found image" />
       </div>
     </div>
   );
