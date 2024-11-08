ef4b128d7ec69a105098ad407c22ac94d33b64f5
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-11-08 19:11:41 +0430

    the dashboard has been optimzed so it sould render the first and last name of the logged in user and also the email of the user

--- client/src/components/features/sidebar/job-seeker/JobSeekerSidebar.jsx
+++ client/src/components/features/sidebar/job-seeker/JobSeekerSidebar.jsx
@@ -1,14 +1,17 @@
 import Sidebar from "@reusable/Sidebar";
 import jobSeekerSidebarData from "@data/sidebar/jobSeekerSidebarData";
 import profileImage from "@images/navbar/profile.png";
+import useUserContext from "@hooks/useUserContext";
 
 export default function JobSeekerSidebar() {
+  const { jobSeeker } = useUserContext();
+
   return (
     <Sidebar
       links={jobSeekerSidebarData}
       avatarImage={profileImage}
-      avatarHeading="Abdulwase Naeemi"
-      avatarParagraph="abdulwasenaeemi7@gmail.com"
+      avatarHeading={`${jobSeeker.firstName} ${jobSeeker.lastName}`}
+      avatarParagraph={jobSeeker.email}
     />
   );
 }
