782206aaf17c96c8c08b72a4722a7b0897d67980
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-11-09 18:43:07 +0430

    the problem with login has been fixed

---
+++ client/src/components/auth/logout/JobSeekerLogout.jsx
@@ -0,0 +1,8 @@
+import UserLogout from "@reusable/UserLogout";
+
+const JobSeekerLogout = () => {
+  // Reusable Component that handles user logout based on user type
+  return <UserLogout userType="jobSeeker" />;
+};
+
+export default JobSeekerLogout;
---
+++ client/src/components/auth/logout/RecruiterLogout.jsx
@@ -0,0 +1,8 @@
+import UserLogout from "@reusable/UserLogout";
+
+const RecruiterLogout = () => {
+  // Reusable Component that handles user logout based on user type
+  return <UserLogout userType="recruiter" />;
+};
+
+export default RecruiterLogout;
