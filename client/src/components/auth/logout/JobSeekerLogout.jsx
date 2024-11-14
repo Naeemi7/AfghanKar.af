fd72cd0dce7a898fce0be3d2a0b43ebddf56009d
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-11-14 13:49:27 +0430

    bug fixed

--- client/src/components/auth/logout/JobSeekerLogout.jsx
+++
@@ -1,8 +0,0 @@
-import UserLogout from "@reusable/UserLogout";
-
-const JobSeekerLogout = () => {
-  // Reusable Component that handles user logout based on user type
-  return <UserLogout userType="jobSeeker" />;
-};
-
-export default JobSeekerLogout;
