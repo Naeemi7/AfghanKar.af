44e058e74fc893ec2db878dabc45c8b5d64f9c6c
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-11-16 18:39:25 +0430

    the problem with navigation of the Recruiter registration layout has been fixed

--- client/src/layouts/RecruiterRegistrationLayout.jsx
+++ client/src/layouts/RecruiterRegistrationLayout.jsx
@@ -3,18 +3,24 @@ import "@styles/layouts/layouts.scss";
 import "@styles/components/recruiter-registration.scss";
 import RecruiterRegistrationSidebar from "@auth/registration/recruiter/RecruiterRegistrationSidebar";
 import MainContent from "@auth/registration/recruiter/MainContent";
+import useNavigation from "@hooks/useNavigation";
 
 export default function RecruiterRegistrationLayout() {
   const [currentStep, setCurrentStep] = useState(1);
+  const { goTo } = useNavigation();
 
   // Handles moving forward to the next step
   const handleNext = () => {
     if (currentStep < 3) setCurrentStep((prevStep) => prevStep + 1);
   };
 
-  // Handles moving backward one step at a time
+  // Handles moving backward or navigating to home if on the first step
   const handlePrevious = () => {
-    if (currentStep > 1) setCurrentStep((prevStep) => prevStep - 1);
+    if (currentStep === 1) {
+      goTo("/registration");
+    } else {
+      setCurrentStep((prevStep) => prevStep - 1);
+    }
   };
 
   return (
