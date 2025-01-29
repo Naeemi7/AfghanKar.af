06229377db18d20579f8cb20caf830ba9954c162
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2025-01-29 15:57:17 +0430

    the problem with the contact page has been resolved

--- client/src/api/emailJs/send-email.js
+++ client/src/api/emailJs/send-email.js
@@ -4,17 +4,19 @@ export default async function sendEmail(name, email, message) {
   const emailJsEndpoint = "https://api.emailjs.com/api/v1.0/email/send";
 
   const payload = {
-    service_id: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-    template_id: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-    user_id: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+    service_id: import.meta.env.VITE_SERVICE_ID,
+    template_id: import.meta.env.VITE_TEMPLATE_ID,
+    user_id: import.meta.env.VITE_PUBLIC_KEY,
     template_params: {
       user_name: name,
       user_email: email,
       message_content: message,
-      your_name: "Abdulwase Naeemi", // Replace with your name or customize
+      your_name: "Abdulwase Naeemi",
     },
   };
 
+  console.log("Public key", import.meta.env.VITE_PUBLIC_KEY);
+  console.log("Template ID ", import.meta.env.VITE_TEMPLATE_ID);
   try {
     const response = await axios.post(emailJsEndpoint, payload, {
       headers: {
@@ -22,7 +24,7 @@ export default async function sendEmail(name, email, message) {
       },
     });
 
-    return response.data; // If successful, EmailJS returns status info
+    return response.data;
   } catch (error) {
     console.error(
       "Error sending email",
